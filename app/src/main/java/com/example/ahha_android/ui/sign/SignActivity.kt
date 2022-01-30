d8bd6e411f8aeb93b00038e221bf1db496caa913
heewon <ymcho24@naver.com>
2022-01-30 23:00:01 +0900

    [FEAT] 화면 생성 및 navigation 설정

--- app/src/main/java/com/example/ahha_android/ui/sign/SignActivity.kt
+++ app/src/main/java/com/example/ahha_android/ui/sign/SignActivity.kt
@@ -1,11 +1,12 @@
-package com.example.ahha_android
+package com.example.ahha_android.ui.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ahha_android.R
 
-class MainActivity : AppCompatActivity() {
+class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign)
     }
 }
