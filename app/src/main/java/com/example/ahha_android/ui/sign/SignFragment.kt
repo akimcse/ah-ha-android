f9a2f8cd6cb6fc353d816998311809da06214334
Hyuna Kim <akxmcse@gmail.com>
2022-02-03 19:08:11 +0900

    [FEAT] sign뷰 레이아웃 구현

--- app/src/main/java/com/example/ahha_android/ui/sign/SignFragment.kt
+++ app/src/main/java/com/example/ahha_android/ui/sign/SignFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.ahha_android.R
+import com.example.ahha_android.databinding.FragmentSignBinding
 
 class SignFragment : Fragment() {
+    private lateinit var binding: FragmentSignBinding
+    private val viewModel: SignViewModel by viewModels()
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign, container, false)
+    ): View {
+        binding = FragmentSignBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.buttonSignUp.setOnClickListener{
+            navController.navigate(R.id.actionSignFragmentToSignPlantFragment)
+        }
     }
 }
