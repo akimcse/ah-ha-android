d710979cce0f706b8c4ad5405b4790459fe58565
heewon <ymcho24@naver.com>
2022-02-03 22:08:49 +0900

    [FEAT] 설정 화면 레이아웃 구현

--- app/src/main/java/com/example/ahha_android/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/ahha_android/ui/setting/SettingFragment.kt
@@ -1,19 +1,20 @@
 package com.example.ahha_android.ui.setting
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ahha_android.R
+import androidx.fragment.app.Fragment
+import com.example.ahha_android.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
+    private lateinit var binding: FragmentSettingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
