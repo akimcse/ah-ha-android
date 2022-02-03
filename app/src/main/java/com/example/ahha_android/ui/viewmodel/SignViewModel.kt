45d515eafe58783988821a30a198605e090e9982
Hyuna Kim <akxmcse@gmail.com>
2022-02-04 04:10:20 +0900

    [FEAT] SignPlant 캐릭터 선택 ViewPager 구현

--- app/src/main/java/com/example/ahha_android/ui/viewmodel/SignViewModel.kt
+++ app/src/main/java/com/example/ahha_android/ui/viewmodel/SignViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.ahha_android.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.ahha_android.R
+import com.example.ahha_android.data.vo.SignPlantData
 
-class SignViewModel : ViewModel() {
+class SignViewModel(application: Application) : AndroidViewModel(application) {
+    private val _characterList = MutableLiveData<List<SignPlantData>>()
+    val characterList : LiveData<List<SignPlantData>>
+        get() = _characterList
+
+
+    fun setCharacterList() {
+        _characterList.value = mutableListOf(
+            SignPlantData("브로콜리", R.drawable.ic_launcher_background),
+            SignPlantData("토마토", R.drawable.ic_launcher_background),
+            SignPlantData("대파", R.drawable.ic_launcher_background),
+            SignPlantData("당근", R.drawable.ic_launcher_background),
+            SignPlantData("양파", R.drawable.ic_launcher_background)
+        )
+    }
 }
