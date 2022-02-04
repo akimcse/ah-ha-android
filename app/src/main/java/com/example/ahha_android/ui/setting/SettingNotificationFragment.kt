f8ecf6aecbf2a1dead8cc74e94496309ce17bc38
heewon <ymcho24@naver.com>
2022-02-04 15:31:32 +0900

    [FEAT] SettingViewModel 생성 및 이메일 삭제 알림의 switch 기능 구현

--- app/src/main/java/com/example/ahha_android/ui/setting/SettingNotificationFragment.kt
+++ app/src/main/java/com/example/ahha_android/ui/setting/SettingNotificationFragment.kt
@@ -5,16 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.ahha_android.databinding.FragmentSettingNotificationBinding
+import com.example.ahha_android.ui.viewmodel.SettingViewModel
 
 class SettingNotificationFragment : Fragment() {
     private lateinit var binding: FragmentSettingNotificationBinding
+    private val viewModel: SettingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSettingNotificationBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
     }
 }
