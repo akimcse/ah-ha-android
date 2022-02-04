eff4f20e1afed7c3f9793604b71efc5a4aceed30
Hyuna Kim <akxmcse@gmail.com>
2022-02-05 03:03:35 +0900

    [FEAT] fragment간 캐릭터 이미지 전달 및 버튼 활성화
    이슈 2가지 있음
    1. viewpager 스크롤하지 않으면 색상은 변하지 않지만 버튼 클릭은 됨
    2. 이름 입력/미입력 구분이 안 먹음 -> 일단 항상 활성화 상태임

--- app/src/main/java/com/example/ahha_android/ui/sign/SignPlantNameFragment.kt
+++ app/src/main/java/com/example/ahha_android/ui/sign/SignPlantNameFragment.kt
@@ -2,6 +2,7 @@ package com.example.ahha_android.ui.sign
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,7 @@ import com.example.ahha_android.R
 import com.example.ahha_android.databinding.FragmentSignPlantNameBinding
 import com.example.ahha_android.ui.main.MainActivity
 import com.example.ahha_android.ui.viewmodel.SignViewModel
+import com.example.ahha_android.util.BindingAdapter.setDrawableImage
 
 class SignPlantNameFragment : Fragment() {
     private lateinit var binding: FragmentSignPlantNameBinding
@@ -25,15 +27,39 @@ class SignPlantNameFragment : Fragment() {
         binding = FragmentSignPlantNameBinding.inflate(inflater, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        setCharacterImage()
+        checkInputBlank()
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun setCharacterImage(){
+        val characterNum = arguments?.getInt("characterNum")
 
+        when(characterNum){
+            1 -> {
+                binding.imageViewCharacter.setDrawableImage(R.drawable.ic_launcher_foreground)
+                Log.d("ViewPagerFragment", "Page $characterNum")
+            }
+            2 -> {
+                binding.imageViewCharacter.setDrawableImage(R.drawable.ic_launcher_background)
+                Log.d("ViewPagerFragment", "Page $characterNum")
+            }
+            3 -> {
+                binding.imageViewCharacter.setDrawableImage(R.drawable.ic_launcher_foreground)
+                Log.d("ViewPagerFragment", "Page $characterNum")
+            }
+        }
+    }
+
+    // 입력/미입력 구분이 안 먹음 -> 일단 항상 활성화 상태임
+    private fun checkInputBlank(){
+        binding.buttonFinish.isActivated = true
         binding.buttonFinish.setOnClickListener{
             val intent = Intent(activity, MainActivity::class.java)
             startActivity(intent)
         }
+        //if(binding.editTextCharacterName.text.isNotBlank()){ }
     }
 }
