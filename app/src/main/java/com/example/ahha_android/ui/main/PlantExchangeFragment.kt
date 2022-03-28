8dcdf35c73a16d5bf500e3456a6e0facd667e5b8
Hyuna Kim <akxmcse@gmail.com>
2022-03-28 16:32:07 +0900

    [feat] 식물 숫자 카운트 로직 구현

--- app/src/main/java/com/example/ahha_android/ui/main/PlantExchangeFragment.kt
+++ app/src/main/java/com/example/ahha_android/ui/main/PlantExchangeFragment.kt
@@ -19,14 +19,67 @@ class PlantExchangeFragment : Fragment() {
         binding = FragmentPlantExchangeBinding.inflate(inflater, container, false)
 
         setImage()
+        countBroccoliNum()
+        countGreenOnionNum()
+        countTomatoNum()
 
         return binding.root
     }
-
 
     private fun setImage() {
         binding.imageViewBroccoli.clipToOutline = true
         binding.imageViewGreenOnion.clipToOutline = true
         binding.imageViewTomato.clipToOutline = true
+    }
+
+    private fun countBroccoliNum(){
+        val broccoliNum = binding.textViewBroccoliNum
+        var num = 0;
+
+        binding.textViewBroccoliMinus.setOnClickListener{
+            if(num > 0){
+                num--
+            }
+            broccoliNum.text = num.toString()
+        }
+
+        binding.textViewBroccoliPlus.setOnClickListener{
+            num++
+            broccoliNum.text = num.toString()
+        }
+    }
+
+    private fun countGreenOnionNum(){
+        val greenOnionNum = binding.textViewGreenOnionNum
+        var num = 0;
+
+        binding.textViewGreenOnionMinus.setOnClickListener{
+            if(num > 0){
+                num--
+            }
+            greenOnionNum.text = num.toString()
+        }
+
+        binding.textViewGreenOnionPlus.setOnClickListener{
+            num++
+            greenOnionNum.text = num.toString()
+        }
+    }
+
+    private fun countTomatoNum(){
+        val tomatoNum = binding.textViewTomatoNum
+        var num = 0;
+
+        binding.textViewTomatoMinus.setOnClickListener{
+            if(num > 0){
+                num--
+            }
+            tomatoNum.text = num.toString()
+        }
+
+        binding.textViewTomatoPlus.setOnClickListener{
+            num++
+            tomatoNum.text = num.toString()
+        }
     }
 }
