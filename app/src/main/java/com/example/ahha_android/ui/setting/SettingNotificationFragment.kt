7e7ca0246ba080f370f5d02f1ad4e3475da7e69e
heewon <ymcho24@naver.com>
2022-03-30 17:49:57 +0900

    [feat] 설정 완료 버튼 클릭 시 토스트 메시지 추가

--- app/src/main/java/com/example/ahha_android/ui/setting/SettingNotificationFragment.kt
+++ app/src/main/java/com/example/ahha_android/ui/setting/SettingNotificationFragment.kt
@@ -5,14 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
 import com.example.ahha_android.R
 import com.example.ahha_android.databinding.FragmentSettingNotificationBinding
 import com.example.ahha_android.ui.viewmodel.SettingViewModel
 import com.example.ahha_android.util.setStatusBarColor
-import kotlin.properties.Delegates
 
 class SettingNotificationFragment : Fragment() {
     private lateinit var binding: FragmentSettingNotificationBinding
@@ -63,6 +62,7 @@ class SettingNotificationFragment : Fragment() {
         val notificationString = arguments?.getString("notificationString")
 
         binding.textViewComplete.setOnClickListener {
+            Toast.makeText(requireContext(), "Modifications Completed!", Toast.LENGTH_SHORT).show()
             Log.d("***********************", "send to server")
             Log.d("*********notificationOn", notificationString.toString())
             Log.d("******notificationLimit", notificationLimit.toString())
